9226b25f8535485dc0da8a0bd5bf41a7fd7a3964
Lauren Arfaras <larfaras96@gmail.com>
2017-09-29 13:39:16 -0400

    questions 1-3 are done; implemented dfs, bfs, and ucs

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -87,17 +87,155 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    start = problem.getStartState()
+
+    # create fringe stack and visited list
+    fringe = util.Stack()
+    visited = []
+
+    # push the start node on the fringe stack
+    fringe.push((start, [], 1))
+
+    while not fringe.isEmpty():
+      n = fringe.pop()
+      actions = n[1]
+
+      visited.append(n[0])
+
+      # check if goal state found
+      if problem.isGoalState(n[0]):
+        return actions
+
+      for successor in problem.getSuccessors(n[0]):
+          if not successor[0] in visited:
+              successorActions = list(n[1])
+              successorActions.append(successor[1])
+              #print "add to fringe: ", (successor[0], testAct, successor[2])
+              fringe.push((successor[0], successorActions, successor[2]))
+
+    return actions
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    start = problem.getStartState()
+
+    # create fringe queue and visited list
+    fringe = util.Queue()
+    visited = []
+    succeeded = []
+
+    # push the start node on the fringe queue
+    fringe.push((start, [], 1))
+
+    while not fringe.isEmpty():
+        n = fringe.pop()
+        actions = n[1]
+
+        visited.append(n[0])
+
+        # check if goal state found
+        if problem.isGoalState(n[0]):
+            return actions
+
+        for successor in problem.getSuccessors(n[0]):
+            if not successor[0] in visited:
+                if not successor[0] in succeeded:
+                    succeeded.append(successor[0])
+                    successorActions = list(n[1])
+                    successorActions.append(successor[1])
+                    #print "add to fringe: ", (successor[0], successorActions, successor[2])
+                    fringe.push((successor[0], successorActions, successor[2]))
+
+    return actions
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    start = problem.getStartState()
+
+    # create fringe queue and visited list
+    fringe = util.PriorityQueue()
+    visited = []
+    succeeded = []
+
+    # push the start node on the fringe queue
+    fringe.push((start, [], 0), 0)
+
+    while not fringe.isEmpty():
+        n = fringe.pop()
+        print "node: ", n
+        actions = n[1]
+
+        visited.append(n[0])
+
+        # check if goal state found
+        if problem.isGoalState(n[0]):
+            return actions
+
+        #print "successors: ", problem.getSuccessors(n[0])
+        #print "current action on node: ", n[1]
+
+        for successor in problem.getSuccessors(n[0]):
+            #print "successors on %s : %s" % (str(n[0]), problem.getSuccessors(n[0]))
+            if not successor[0] in visited:
+                if (not successor[0] in succeeded) or (problem.isGoalState(successor[0])):
+                    succeeded.append(successor[0])
+                    successorActions = list(n[1])
+                    successorActions.append(successor[1])
+                    successorCost = n[2] + successor[2]
+                    #print "pushing: ", (successor[0], successorCost)
+                    fringe.push((successor[0], successorActions, successorCost), successorCost)
+        #print "went through successors"
+
+        # check if goal state found
+        if problem.isGoalState(n[0]):
+            return actions
+
+    return actions
+
+    """start = problem.getStartState()
+
+    #print "Start: ", start
+    #print "Is the start a goal?", problem.isGoalState(start)
+    #print "Start's successors: ", problem.getSuccessors(start)
+
+    # create fringe queue and visited list
+    fringe = util.PriorityQueue()
+    visited = []
+    actions = []
+
+    # if current state is goal state then return no actions
+    if problem.isGoalState(start):
+        return actions
+
+    # push the start node on the fringe queue
+    fringe.push((start, [], 1), 1)
+
+    while not fringe.isEmpty():
+        n = fringe.pop()
+        actions = n[1]
+        #print "actions 1: ", actions
+
+        # check if goal path found
+        if problem.isGoalState(n[0]):
+            #print "actions: ", actions
+            return actions
+
+        for successor in problem.getSuccessors(n[0]):
+            actions = list(n[1])
+            if not (successor[0] in visited):
+                visited.append(successor[0])
+                #print "successor: ", successor
+                actions.append(successor[1])
+                fringe.push((successor[0], actions, successor[2]), successor[2])
+
+    print "actions: ", actions
+    return actions
+    #util.raiseNotDefined()"""
 
 def nullHeuristic(state, problem=None):
     """
